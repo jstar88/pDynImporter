97b6bdd55075572d805596f08e119b434d3f6759
jstar88 <covolo.n@libero.it>
2015-02-05 21:10:50 +0100

    fixed example

--- tests/main.py
+++ tests/main.py
@@ -1,11 +1,10 @@
 from pDynImporter import checkImports
 import __builtin__
 
+modules = [ ('test',['x']) ]
+checkImports(modules)
 for k,v in  __builtin__.modules.iteritems():
     if not k.startswith("_"):
         globals()[k] = v
-
-modules = [ ('test',['x']) ]
-checkImports(modules)
     
 x()
